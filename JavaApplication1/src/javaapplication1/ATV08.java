4950eb3e4f87385b1cf7f4cf54e9d111adde9d6c
guilherme oliveira <guilherme.nascimento@sga.pucminas.br>
2026-08-24 13:09:11 -0300

    ATV08 FINALIZADA

---
+++ JavaApplication1/src/javaapplication1/ATV08.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class ATV08 {
+
+    
+    public static boolean senhaValida(String senha) { // verifica se a senha e valida
+
+        
+        if (senha.length() < 8) {
+            return false;  // senha precisa ter pelo menos 8 caracteres
+        }
+   
+        boolean maiuscula = false;
+        boolean minuscula = false;
+        boolean numero = false;
+        boolean especial = false;
+
+        
+        for (int i = 0; i < senha.length(); i++) {
+            char caractere = senha.charAt(i);
+
+            if (caractere >= 'A' && caractere <= 'Z') {
+                maiuscula = true; // encontrou uma letra maiuscula
+            } else if (caractere >= 'a' && caractere <= 'z') {
+                minuscula = true; // encontrou uma letra minuscula
+            } else if (caractere >= '0' && caractere <= '9') {
+                numero = true; // encontrou um numero
+            } else {
+                especial = true; // se nao for letra ou numero, e especial
+            }
+        }
+
+        
+        
+        return maiuscula && minuscula && numero && especial; // retorna o valor de todas, pela logica
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNextLine()) {
+
+            String senha = sc.nextLine();
+
+            if (senhaValida(senha)) {
+                System.out.println("SIM");           // chama a funcao e printa na tela o resultado
+            } else {
+                System.out.println("NAO");
+            }
+        }
+
+        sc.close();
+    }
+}
